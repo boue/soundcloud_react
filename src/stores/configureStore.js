65f75de9805a4ac420483983dc7758f481f6912b
Germain Boue <boue.germain@gmail.com>
2016-06-28 17:52:33 -0700

    Moreover we sync our store with the browser history, so that we can listen later on to events based on our current route.Properties like browser path or query params in the URL can be accessed in the store now.

--- src/stores/configureStore.js
+++ src/stores/configureStore.js
@@ -1,8 +1,11 @@
 import { createStore, applyMiddleware } from 'redux';
 import createLogger from 'redux-logger';
+import { browserHistory } from 'react-router';
+import { routerMiddleware } from 'react-router-redux';
 import rootReducer from '../reducers/index';
 
 const logger = createLogger();
+const router = routerMiddleware(browserHistory);
 
 const createStoreWithMiddleware = applyMiddleware(logger)(createStore);
 
